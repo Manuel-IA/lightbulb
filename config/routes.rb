7c5703d75e368d60e559ec3401b464eb81abb69d
Manuel Esteban Palacio Calderón <manuelpalacio@wss.com.co>
2022-07-15 15:03:16 -0500

    Added routes from controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'bulb', to: 'bulbs#index'
+  get 'change', to: 'bulbs#change'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
